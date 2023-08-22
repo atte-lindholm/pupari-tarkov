527b8741f4ed72d017ebd5523962370e40bede50
brain-dead-sodium-chloride <gr277685@gradia.fi>
2023-08-22 12:15:10 +0300

    enemy go brr brr

--- EscapeFromPupari/Assets/Scripts/GunThings/GunSway.cs
+++ EscapeFromPupari/Assets/Scripts/GunThings/GunSway.cs
@@ -27,12 +27,12 @@ public class GunSway : MonoBehaviour
     private void UpdateGunSway() 
     {
         // mouse controlls
-        float t_x_mouse = Input.GetAxis("Mouse X");
-        float t_y_mouse = Input.GetAxis("Mouse Y");
+        float x_mouse = Input.GetAxis("Mouse X");
+        float y_mouse = Input.GetAxis("Mouse Y");
 
         //target rotation
-        Quaternion t_x_adj = Quaternion.AngleAxis(-intensity * t_x_mouse, Vector3.up);
-        Quaternion t_y_adj = Quaternion.AngleAxis(intensity * t_y_mouse, Vector3.right);
+        Quaternion t_x_adj = Quaternion.AngleAxis(-intensity * x_mouse, Vector3.up);
+        Quaternion t_y_adj = Quaternion.AngleAxis(intensity * y_mouse, Vector3.right);
         Quaternion target_rotation = original_rotation * t_x_adj * t_y_adj;
 
         //rotate towards target rotation
