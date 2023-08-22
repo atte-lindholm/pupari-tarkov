c10127c16bd8f84418103d90cd1a16bd2f43e368
Tomi Vahdersalo <tomi.vahdersalo@gmail.com>
2023-08-22 11:07:48 +0300

    foodbreak

--- EscapeFromPupari/Assets/Scripts/FirstPersonCamera.cs
+++ EscapeFromPupari/Assets/Scripts/FirstPersonCamera.cs
@@ -17,10 +17,6 @@ public class Firstplayercamera : MonoBehaviour
     float cameraVertivalRotation = 0f;
     private Transform _playerTransform;
     private Rigidbody _player;
-    bool lockedCursor = true;
-    private bool woblleright = true;
-    private float i = 1.0f;
-    private float test;
 
     // Start is called before the first frame update
     void Start()
@@ -29,55 +25,20 @@ public class Firstplayercamera : MonoBehaviour
         _player = _playerTransform.GetComponent<Rigidbody>();
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
-
     }
 
     // Update is called once per frame
     void Update()
     {
+
         //get mouse input
         float inputX = Input.GetAxis("Mouse X") * mouseSensitivity;
         float inputY = Input.GetAxis("Mouse Y") * mouseSensitivity;
 
-        
-        var xspeed = _player.velocity.x;
-        var zspeed = _player.velocity.z;
+
         Vector3 euler = transform.localEulerAngles;
 
 
-        //if (zspeed > 3 || zspeed < -3)
-        //{
-        //    if (!Input.GetKey(KeyCode.C))
-        //    {
-        //        if (woblleright == true)
-        //        {
-        //            i = i + rotationSpeed / 10;
-        //            if (i > rotationAmount)
-        //            {
-        //                woblleright = false;
-
-        //            }
-
-        //        }
-        //        else
-        //        {
-        //            i = i - rotationSpeed / 10;
-        //            if (i < -rotationAmount)
-        //            {
-        //                woblleright = true;
-        //            }
-
-        //        }
-        //        euler.z = ((zspeed * walkRotation * i)+test)/2;
-
-        //    }
-        //    else { euler.z = euler.z / 2; }
-        //}
-        euler.z = rotation * zspeed;
-
-        test = euler.z;
-
-            
         //camera rotation Y axis
         cameraVertivalRotation -= inputY;
         cameraVertivalRotation = Mathf.Clamp(cameraVertivalRotation, -90f, 90f);
