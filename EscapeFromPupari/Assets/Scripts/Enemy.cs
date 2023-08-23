4b310e4c9eb68ff9b1e9514c9306f9c9557c58e8
brain-dead-sodium-chloride <gr277685@gradia.fi>
2023-08-23 09:40:57 +0300

    enemy code fixed

--- EscapeFromPupari/Assets/Scripts/Enemy.cs
+++ EscapeFromPupari/Assets/Scripts/Enemy.cs
@@ -1,51 +1,59 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
 {
     public int maxHealth = 100;
-private int currentHealth;
+    private int currentHealth;
 
-public float speed = 5.0f; // Constant speed
-public Transform startPoint;
-public Transform endPoint;
+    public float speed = 5.0f; // Constant speed
+    public Transform startPoint;
+    public Transform endPoint;
 
-private bool movingToEnd = true;
+    private bool movingToEnd = true;
 
-private void Update()
-{
-    if (movingToEnd)
+    private void Update()
     {
-        // Move towards the end point
-        transform.position = Vector3.MoveTowards(transform.position, endPoint.position, speed * Time.deltaTime);
-
-        // Check if reached the end point
-        if (Vector3.Distance(transform.position, endPoint.position) < 0.1f)
+        if (movingToEnd)
         {
-            // Turn around
-            Flip();
+            // Move towards the end point
+            transform.position = Vector3.MoveTowards(transform.position, endPoint.position, speed * Time.deltaTime);
+
+            // Check if reached the end point
+            if (Vector3.Distance(transform.position, endPoint.position) < 0.1f)
+            {
+                // Turn around
+                Flip();
+            }
+        }
+        else
+        {
+            // Move towards the start point
+            transform.position = Vector3.MoveTowards(transform.position, startPoint.position, speed * Time.deltaTime);
+
+            // Check if reached the start point
+            if (Vector3.Distance(transform.position, startPoint.position) < 0.1f)
+            {
+                // Turn around
+                Flip();
+            }
         }
     }
-    else
+
+    // This function flips the enemy's direction
+    private void Flip()
     {
-        // Move towards the start point
-        transform.position = Vector3.MoveTowards(transform.position, startPoint.position, speed * Time.deltaTime);
-
-        // Check if reached the start point
-        if (Vector3.Distance(transform.position, startPoint.position) < 0.1f)
-        {
-            // Turn around
-            Flip();
-        }
+        movingToEnd = !movingToEnd;
+        transform.Rotate(Vector3.up, 180f); // Rotate 180 degrees
     }
-}
 
-// This function flips the enemy's direction
-private void Flip()
-{
-    movingToEnd = !movingToEnd;
-    transform.Rotate(Vector3.up, 180f); // Rotate 180 degrees
-}
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
 
-public void TakeDamage(int damage)
-    @@ -20,5 + 56,4 @@ public void TakeDamage(int damage)
+        // Add any additional logic for handling damage here
+        if (currentHealth <= 0)
+        {
             Destroy(gameObject);
         }
     }
-} 
+}
